8301cb1379a19927df12926a8b89107e1113068f
Ido Klimovsky <ido.klimovsky@adesso.de>
2018-11-07 09:26:21 +0100

    changed spielregel

--- src/main/java/Komponenten/SpielregelKomponent/ISpielregelKomponent.java
+++ src/main/java/Komponenten/SpielregelKomponent/ISpielregelKomponent.java
@@ -5,12 +5,5 @@ import datenmodel.Spielkarte;
 
 public interface ISpielregelKomponent {
 
-    boolean konfigurierRegelFuerSpiel(Boolean zwei_ziehen_aktiv, Spielkarte zwei_ziehen_karte,
-                                      Boolean aussetzen_aktiv, Spielkarte aussetzen_karte,
-                                      Boolean wuenscher_aktiv, Spielkarte wuenscher_karte,
-                                      Boolean richtungswechsel_aktiv, Spielkarte richtungswechsel_karte,
-                                      Boolean stopper_aktiv, Spielkarte stopper_karte,
-                                      Boolean allesleger_aktiv, Spielkarte allesleger_karte);
-
     Regel getRegelFuerKarte(Spielkarte spielkarte);
 }
