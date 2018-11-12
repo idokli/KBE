220dd05ef3e5a1f3a88d9fffc6ba39e658fdc5db
Ido Klimovsky <ido.klimovsky@adesso.de>
2018-11-12 10:59:46 +0100

    added exceptions to the steuerung

--- src/main/java/Komponenten/SpielSteuerung/export/ISpielSteuerung.java
+++ src/main/java/Komponenten/SpielSteuerung/export/ISpielSteuerung.java
@@ -1,6 +1,7 @@
 package Komponenten.SpielSteuerung.export;
 
 import datenmodel.Enum.Blatttyp;
+import datenmodel.Exceptions.MauMauException;
 import datenmodel.Spieler;
 import datenmodel.Spielkarte;
 import datenmodel.Spielrunde;
@@ -12,50 +13,57 @@ public interface ISpielSteuerung {
     /**
      * Um bei der Spielsteuerung zu fragen, welcher Spieler daran ist
      * @return
+     * @throws MauMauException
      */
-    Spieler fragWerDaranIst();
+    Spieler fragWerDaranIst() throws MauMauException;
 
     /**
      * Prüft, ob der Spieler, der daran ist, Karten zu ziehen hat
      * @param spielrunde
      * @return
+     * @throws MauMauException
      */
-    int checkZuziehendenKarten(Spielrunde spielrunde);
+    int checkZuziehendenKarten(Spielrunde spielrunde) throws MauMauException;
 
     /**
      * Prüft ob Spielkarte auflegbar ist
      * @param spieler
      * @param spielkarte
      * @return
+     * @throws MauMauException
      */
-    boolean spieleKarte(Spieler spieler, Spielkarte spielkarte);
+    boolean spieleKarte(Spieler spieler, Spielkarte spielkarte) throws MauMauException;
 
     /**
      * Prüft ob der Spieler, der daran ist, Mau Mau aufrufen sollte
      * @param spieler
      * @return
+     * @throws MauMauException
      */
-    boolean sollMauMauAufrufen(Spieler spieler);
+    boolean sollMauMauAufrufen(Spieler spieler) throws MauMauException;
 
 
     /**
      * Prüft, ob der Spieler, der daran ist, sich einen Blatttyp aussuchen kann (wegen Regel "Wünscher")
      * @return
+     * @throws MauMauException
      */
-    boolean pruefeObWuenscher(Spielkarte spielkarte);
+    boolean pruefeObWuenscher(Spielkarte spielkarte) throws MauMauException;
 
     /**
      * Der Spielerwünsch wird übergeben
      * @param blatttyp
+     * @throws MauMauException
      */
-    void bestimmeBlatttyp(Blatttyp blatttyp);
+    void bestimmeBlatttyp(Blatttyp blatttyp) throws MauMauException;
 
     /**
      * Spielsteuerung zieht eine Spielkarte vom Kartenstapel
      * @param spieler
      * @param anzahlKarten
      * @return List<Spielkarte>
+     * @throws MauMauException
      */
-    Spieler zieheKartenVomStapel(Spieler spieler, int anzahlKarten);
+    Spieler zieheKartenVomStapel(Spieler spieler, int anzahlKarten) throws MauMauException;
 
 }
