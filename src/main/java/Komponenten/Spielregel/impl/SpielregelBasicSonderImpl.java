cffaa01226c6df195f5b46f22a9fa7f4939735dd
vicdev89 <victor.del.campo@student.htw-berlin.de>
2018-12-01 14:59:25 +0100

    Spielregel-Implementierungen umgesetzt

--- src/main/java/Komponenten/Spielregel/impl/SpielregelBasicSonderImpl.java
+++ src/main/java/Komponenten/Spielregel/impl/SpielregelBasicSonderImpl.java
@@ -1,18 +1,76 @@
 package komponenten.Spielregel.impl;
 
+import datenmodel.Enum.Blatttyp;
+import datenmodel.Enum.Blattwert;
+import datenmodel.Exceptions.MauMauException;
+import datenmodel.HilfKlassen.RegelComponentUtil;
+import datenmodel.Spieler;
+import datenmodel.Spielkarte;
 import komponenten.Spielregel.export.ISpielregel;
-
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 /**
- * Implementierung mit normalen Maumau-Regel + Basic Sonderregel
+ * Implementierung von Spielregel-Komponent mit Zwei_ziehen(7), Aussetzen(Ass) und Wünscher(Bube)
  */
 @Service
 @Qualifier("basicSonder")
 public class SpielregelBasicSonderImpl extends SpielregelOhneSonderImpl implements ISpielregel {
 
 
+    @Override
+    public boolean istKarteLegbar(Spielkarte vorherigeSpielkarte, Spielkarte aktuelleSpielkarte, Blatttyp blatttyp) throws MauMauException {
+        // TODO wie behandeln wir exceptions?
+        if (vorherigeSpielkarte == null || aktuelleSpielkarte == null) {
+            throw new MauMauException("Fehler");
+        }
+        // Basic-Prüfung ohne SonderRegel
+        boolean istLegbar = super.istKarteLegbar(vorherigeSpielkarte, aktuelleSpielkarte, blatttyp);
+        // Wenn Blatttyp im Zug davor gewählt, dann kann man nur Blattyp spielen aber nicht noch eine Bube
+        if (blatttyp != null) {
+            if (aktuelleSpielkarte.getBlatttyp() == blatttyp && aktuelleSpielkarte.getBlattwert() == Blattwert.Bube) {
+                istLegbar = false;
+            }
+        } else {
+            // Sonderprüfung wegen Zwei_ziehen
+            if (vorherigeSpielkarte.getBlattwert() == Blattwert.Sieben && aktuelleSpielkarte.getBlattwert() != Blattwert.Sieben) {
+                istLegbar = false;
+            }
+        }
+        return istLegbar;
+    }
 
+    @Override
+    public RegelComponentUtil holeAuswirkungVonKarte(Spielkarte aktuelleSpielkarte, List<Spieler> spielerListe) throws MauMauException {
+        // TODO wie behandeln wir Exceptions?
+        if (aktuelleSpielkarte == null || spielerListe == null) {
+            throw new MauMauException("Fehler");
+        }
+        RegelComponentUtil util = super.holeAuswirkungVonKarte(aktuelleSpielkarte, spielerListe);
+        switch (aktuelleSpielkarte.getBlattwert()) {
+            case Sieben:
+                util.setAnzahlKartenZuZiehen(2);
+                break;
+            case Ass:
+                for (Spieler spieler : spielerListe) {
+                    if (spieler.isSpielend()) {
+                        int indexSpielend = spielerListe.indexOf(spieler);
+                        if (indexSpielend == spielerListe.size() - 1) {
+                            spielerListe.get(1).setSpielend(true);
+                        } else if (indexSpielend == spielerListe.size() - 2) {
+                            spielerListe.get(0).setSpielend(true);
+                        } else {
+                            spielerListe.get(indexSpielend + 1).setSpielend(true);
+                        }
+                        break;
+                    }
+                }
+                util.setSpielerListe(spielerListe);
+                break;
+        }
+        return util;
+    }
 }
--- src/main/java/komponenten/Spielregel/export/ISpielregel.java
+++ src/main/java/komponenten/Spielregel/export/ISpielregel.java
@@ -11,31 +11,32 @@ import java.util.List;
 public interface ISpielregel {
 
     /**
-     * Erste Prüfung ob die vom User ausgewählten Spielkarte überhaupt legbar ist
-     * Blatttyp muss übergeben werden, für den Fall dass einen Blatttyp im Zug davor gewünscht wurde
-     * @param vorherigeSpielkarte
-     * @param aktuelleSpielkarte
-     * @param blatttyp
-     * @return
+     * Prüft ob eine Spielkarte auf eine andere Spielkarte legbar ist
+     *
+     * @param vorherigeSpielkarte - die Spielkarte, die im Zug davor gespielt wurde
+     * @param aktuelleSpielkarte  - die Spielkarte, die im aktuellen Zug gespielt werden soll
+     * @param blatttyp            - der Blatttyp, der bei einer Bube gewünscht wird
+     * @return boolean - ob die Spielkarte legbar ist oder nicht
      * @throws MauMauException
      */
     boolean istKarteLegbar(Spielkarte vorherigeSpielkarte, Spielkarte aktuelleSpielkarte, Blatttyp blatttyp) throws MauMauException;
 
     /**
-     * Bestimmt die Auswirkungen von der gespielten Karte bzw. wer daran ist und wie viele Karten zu ziehen sind
-     * @param aktuelleSpielkarte
-     * @param spielerListe
-     * @return
+     * Bestimmt die Auswirkungen von der gespielten Karte. In dieser Impl gibt es aber keine Sonderegel und daher
+     * wird nur der zu spielende Spieler bestimmt.
+     *
+     * @param aktuelleSpielkarte - die Spielkarte, die im aktuellen Zug gespielt wird
+     * @param spielerListe       - die Spielerliste
+     * @return RegelComponentUtil - Util-Object, um die Spielerliste und die Anzahl von zu ziehenden Karten zurückzugeben
      * @throws MauMauException
      */
-    RegelComponentUtil holeAuswirkungVonKarte(Spielkarte aktuelleSpielkarte, List<Spieler> spielerListe)throws MauMauException;
-
+    RegelComponentUtil holeAuswirkungVonKarte(Spielkarte aktuelleSpielkarte, List<Spieler> spielerListe) throws MauMauException;
 
     /**
-     * Prüft, ob die übergebene Spielkarte die Wünscher-Regel enthält
-     * @param spielkarte
-     * @return
-     * @throws MauMauException
+     * Prüft, ob die übergebene Spielkarte den Blattwert Bube hat
+     *
+     * @param spielkarte - die übergebene Spielkarte
+     * @return boolean - ja oder nein
      */
     boolean pruefeObWuenscher(Spielkarte spielkarte) throws MauMauException;
 }
